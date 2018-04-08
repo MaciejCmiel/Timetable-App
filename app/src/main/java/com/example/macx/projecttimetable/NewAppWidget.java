da92f6b3414a85eea4659e9dd429d27405fb4c82
Maciej Ćmiel <maciej.cmiel.S@ee.pw.edu.pl>
2018-04-08 23:41:37 +0200

    Back to working widget (placeholders)

--- app/src/main/java/com/example/macx/projecttimetable/NewAppWidget.java
+++ app/src/main/java/com/example/macx/projecttimetable/NewAppWidget.java
@@ -4,8 +4,6 @@ import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
 import android.widget.RemoteViews;
 
 import java.util.ArrayList;
@@ -18,7 +16,6 @@ public class NewAppWidget extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        /*
         // tutorialspoint suggestion
         //PendingIntent pending = PendingIntent.getActivity(context, 0, intent, 0);
         String time = "10:00 - 11:00";
@@ -28,7 +25,7 @@ public class NewAppWidget extends AppWidgetProvider {
         String cycle = "I połowa sem.";
 
         // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_subject_list_item);
 
 
         views.setTextViewText(R.id.time_text_view, time);
@@ -40,41 +37,15 @@ public class NewAppWidget extends AppWidgetProvider {
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
-        */
-
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
-            RemoteViews remoteViews = updateWidgetListView(context, appWidgetIds[appWidgetId]);
-            appWidgetManager.updateAppWidget(appWidgetIds[appWidgetId], remoteViews);
+            updateAppWidget(context, appWidgetManager, appWidgetId);
         }
-        super.onUpdate(context, appWidgetManager, appWidgetIds);
     }
-
-    private RemoteViews updateWidgetListView(Context context, int appWidgetId) {
-
-        //which layout to show on widget
-        RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
-                R.layout.new_app_widget);
-
-        //RemoteViews Service needed to provide adapter for ListView
-        Intent svcIntent = new Intent(context, WidgetService.class);
-        //passing app widget id to that RemoteViews Service
-        svcIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
-        //setting a unique Uri to the intent
-        //don't know its purpose to me right now
-        svcIntent.setData(Uri.parse(svcIntent.toUri(Intent.URI_INTENT_SCHEME)));
-        //setting adapter to listview of the widget
-        remoteViews.setRemoteAdapter(appWidgetId, R.id.list,
-                svcIntent);
-        //setting an empty view in case of no data
-        remoteViews.setEmptyView(R.id.list, R.id.empty_view);
-        return remoteViews;
-    }
-
 
     @Override
     public void onEnabled(Context context) {
