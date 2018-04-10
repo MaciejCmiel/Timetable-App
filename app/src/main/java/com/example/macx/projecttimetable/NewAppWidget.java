dfd3e9cd09697ad7ec28b3a1c6fc37bbd0f5c9d1
Maciej Ćmiel <maciej.cmiel.S@ee.pw.edu.pl>
2018-04-10 10:26:54 +0200

    A little colser to working widget

--- app/src/main/java/com/example/macx/projecttimetable/NewAppWidget.java
+++ app/src/main/java/com/example/macx/projecttimetable/NewAppWidget.java
@@ -6,6 +6,7 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.util.Log;
 import android.widget.RemoteViews;
 
 import java.util.ArrayList;
@@ -46,19 +47,24 @@ public class NewAppWidget extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
-        for (int appWidgetId : appWidgetIds) {
-            RemoteViews remoteViews = updateWidgetListView(context, appWidgetIds[appWidgetId]);
-            appWidgetManager.updateAppWidget(appWidgetIds[appWidgetId], remoteViews);
+
+        Log.i(NewAppWidget.class.getName(), "TEST: onUpdate");
+        final int N = appWidgetIds.length;
+		/*int[] appWidgetIds holds ids of multiple instance of your widget
+		 * meaning you are placing more than one widgets on your homescreen*/
+        for (int i = 0; i < N; ++i) {
+            RemoteViews remoteViews = updateWidgetListView(context, appWidgetIds[i]);
+            appWidgetManager.updateAppWidget(appWidgetIds[i], remoteViews);
         }
         super.onUpdate(context, appWidgetManager, appWidgetIds);
     }
 
     private RemoteViews updateWidgetListView(Context context, int appWidgetId) {
 
+        Log.i(NewAppWidget.class.getName(), "TEST: updateWidgetListView");
+
         //which layout to show on widget
-        RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
-                R.layout.new_app_widget);
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
 
         //RemoteViews Service needed to provide adapter for ListView
         Intent svcIntent = new Intent(context, WidgetService.class);
@@ -68,10 +74,9 @@ public class NewAppWidget extends AppWidgetProvider {
         //don't know its purpose to me right now
         svcIntent.setData(Uri.parse(svcIntent.toUri(Intent.URI_INTENT_SCHEME)));
         //setting adapter to listview of the widget
-        remoteViews.setRemoteAdapter(appWidgetId, R.id.list,
-                svcIntent);
+        remoteViews.setRemoteAdapter(appWidgetId, R.id.list, svcIntent);
         //setting an empty view in case of no data
-        remoteViews.setEmptyView(R.id.list, R.id.empty_view);
+        //remoteViews.setEmptyView(R.id.list, R.id.empty_view);
         return remoteViews;
     }
 
--- app/src/main/java/com/example/macx/projecttimetable/WidgetService.java
+++ app/src/main/java/com/example/macx/projecttimetable/WidgetService.java
@@ -2,6 +2,7 @@ package com.example.macx.projecttimetable;
 
 import android.appwidget.AppWidgetManager;
 import android.content.Intent;
+import android.util.Log;
 import android.widget.RemoteViewsService;
 
 public class WidgetService extends RemoteViewsService {
@@ -12,10 +13,8 @@ public class WidgetService extends RemoteViewsService {
 
 	@Override
 	public RemoteViewsFactory onGetViewFactory(Intent intent) {
-		int appWidgetId = intent.getIntExtra(
-				AppWidgetManager.EXTRA_APPWIDGET_ID,
-				AppWidgetManager.INVALID_APPWIDGET_ID);
-
+		int appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+		Log.i(WidgetService.class.getName(), "TEST: SERVICE TESTING");
 		return (new ListProvider(this.getApplicationContext(), intent));
 	}
 
